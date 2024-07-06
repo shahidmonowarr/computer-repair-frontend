10faa6ae4bbc5caa012cd9c6ad1f0debee858abe
shahidmonowarr <shahidmonowar21@gmail.com>
2024-07-06 15:38:01 +0600

    added turbo

--- src/helpers/config/envConfig.ts
+++ src/helpers/config/envConfig.ts
@@ -1,9 +1,9 @@
-export const getBaseUrl = (): string => {
-  return (
-    process.env.NEXT_PUBLIC_API_BASE_URL ||
-    "https://computer-repair-backend.vercel.app/api/v1"
-  );
-};
 // export const getBaseUrl = (): string => {
-//   return "http://localhost:5005/api/v1";
+//   return (
+//     process.env.NEXT_PUBLIC_API_BASE_URL ||
+//     "https://computer-repair-backend.vercel.app/api/v1"
+//   );
 // };
+export const getBaseUrl = (): string => {
+  return "http://localhost:5005/api/v1";
+};
